ee10fce61f3f51f0b94867bac38bd14e251658a7
ozwaldorf <self@ossian.dev>
2026-01-07 20:35:35 -0500

    feat: nixos module providing server as systemd service

--- flake.nix
+++ flake.nix
@@ -39,5 +39,74 @@
           ];
         };
       }
-    );
+    )
+    // {
+      nixosModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config.services.concert-display-server;
+        in
+        {
+          options.services.concert-display-server = {
+            enable = lib.mkEnableOption "Concert Display server";
+
+            port = lib.mkOption {
+              type = lib.types.port;
+              default = 3000;
+              description = "Port to listen on";
+            };
+
+            logLevel = lib.mkOption {
+              type = lib.types.str;
+              default = "info";
+              description = "RUST_LOG filter string";
+            };
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${pkgs.system}.server;
+              description = "The concert-display-server package to use";
+            };
+
+            openFirewall = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+              description = "Whether to open the firewall port";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.port ];
+            systemd.services.concert-display-server = {
+              description = "Concert Display Server";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+
+              environment = {
+                PORT = toString cfg.port;
+                RUST_LOG = cfg.logLevel;
+              };
+
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = "${cfg.package}/bin/concert-display-server";
+                Restart = "on-failure";
+                RestartSec = 5;
+
+                # Hardening
+                DynamicUser = true;
+                NoNewPrivileges = true;
+                ProtectSystem = "strict";
+                ProtectHome = true;
+                PrivateTmp = true;
+                PrivateDevices = true;
+                ProtectKernelTunables = true;
+                ProtectKernelModules = true;
+                ProtectControlGroups = true;
+                RestrictNamespaces = true;
+                RestrictSUIDSGID = true;
+              };
+            };
+          };
+        };
+    };
 }
